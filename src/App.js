696439c1a1795c1257bca4fd7db7c524c6c859f0
Jonathan Alvares <jonthan.alvares@yahoo.com>
2022-04-08 12:43:05 +0530

    Name of the app done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="font-mono text-lg subpixel-antialiased font-bold tracking-wide leading-7 text-center text-lime-100">
+        Quiz App
+      </h1>
+    </div>
+  );
 }
 
 export default App;
