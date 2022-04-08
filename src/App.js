ec368da533a076f2a37fe36974d819336da0f35b
Jonathan Alvares <jonthan.alvares@yahoo.com>
2022-04-08 13:04:31 +0530

    Combined classes into one

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="font-mono text-lg subpixel-antialiased font-bold tracking-wide leading-7 text-center text-lime-100">
-        Quiz App
-      </h1>
+      <h1 className="logo">Quiz App</h1>
     </div>
   );
 }
